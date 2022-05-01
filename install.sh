26133df05f51569a8a5f1328dad86e109035fe0b
beastksoepic <beastk@beastksoepic.com>
2022-05-02 06:54:54 +0700

    added root and curl check

--- install.sh
+++ install.sh
@@ -1,3 +1,16 @@
+# exit with error status code if user is not root
+if [[ $EUID -ne 0 ]]; then
+  echo "* This script must be executed with root privileges (sudo)." 1>&2
+  exit 1
+fi
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
 echo -n "Do you want to install pterodaxyl-theme? y/n "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then
